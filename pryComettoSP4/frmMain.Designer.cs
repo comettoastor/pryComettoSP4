c1d12b21fd9c60212c5d5d3b53712959865d377c
comettoastor <comettoastor@gmail.com>
2022-11-19 01:57:11 -0300

    Agregar archivos de proyecto.

---
+++ pryComettoSP4/frmMain.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace pryComettoSP4
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.dgvMozos = new System.Windows.Forms.DataGridView();
+            this.mozo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Comidas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Bebidas_sin_alcohol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Bebidas_con_alcohol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Postres = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnValidarDatos = new System.Windows.Forms.Button();
+            this.btnTotales = new System.Windows.Forms.Button();
+            this.btnMozoDia = new System.Windows.Forms.Button();
+            this.txtMozoDia = new System.Windows.Forms.TextBox();
+            this.lstTotales = new System.Windows.Forms.ListBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMozos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMozos
+            // 
+            this.dgvMozos.AllowUserToAddRows = false;
+            this.dgvMozos.AllowUserToDeleteRows = false;
+            this.dgvMozos.AllowUserToResizeColumns = false;
+            this.dgvMozos.AllowUserToResizeRows = false;
+            this.dgvMozos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMozos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.mozo,
+            this.Comidas,
+            this.Bebidas_sin_alcohol,
+            this.Bebidas_con_alcohol,
+            this.Postres});
+            this.dgvMozos.Location = new System.Drawing.Point(12, 12);
+            this.dgvMozos.Name = "dgvMozos";
+            this.dgvMozos.RowHeadersVisible = false;
+            this.dgvMozos.Size = new System.Drawing.Size(503, 146);
+            this.dgvMozos.TabIndex = 0;
+            // 
+            // mozo
+            // 
+            this.mozo.HeaderText = "Mozo";
+            this.mozo.Name = "mozo";
+            this.mozo.ReadOnly = true;
+            // 
+            // Comidas
+            // 
+            this.Comidas.HeaderText = "Comidas";
+            this.Comidas.Name = "Comidas";
+            // 
+            // Bebidas_sin_alcohol
+            // 
+            this.Bebidas_sin_alcohol.HeaderText = "Bebidas sin alcohol";
+            this.Bebidas_sin_alcohol.Name = "Bebidas_sin_alcohol";
+            // 
+            // Bebidas_con_alcohol
+            // 
+            this.Bebidas_con_alcohol.HeaderText = "Bebidas con alcohol";
+            this.Bebidas_con_alcohol.Name = "Bebidas_con_alcohol";
+            // 
+            // Postres
+            // 
+            this.Postres.HeaderText = "Postres";
+            this.Postres.Name = "Postres";
+            // 
+            // btnValidarDatos
+            // 
+            this.btnValidarDatos.Location = new System.Drawing.Point(407, 164);
+            this.btnValidarDatos.Name = "btnValidarDatos";
+            this.btnValidarDatos.Size = new System.Drawing.Size(106, 41);
+            this.btnValidarDatos.TabIndex = 1;
+            this.btnValidarDatos.Text = "Validar Datos";
+            this.btnValidarDatos.UseVisualStyleBackColor = true;
+            this.btnValidarDatos.Click += new System.EventHandler(this.btnValidarDatos_Click);
+            // 
+            // btnTotales
+            // 
+            this.btnTotales.Enabled = false;
+            this.btnTotales.Location = new System.Drawing.Point(267, 224);
+            this.btnTotales.Name = "btnTotales";
+            this.btnTotales.Size = new System.Drawing.Size(106, 41);
+            this.btnTotales.TabIndex = 2;
+            this.btnTotales.Text = "Totales";
+            this.btnTotales.UseVisualStyleBackColor = true;
+            this.btnTotales.Click += new System.EventHandler(this.btnTotales_Click);
+            // 
+            // btnMozoDia
+            // 
+            this.btnMozoDia.Enabled = false;
+            this.btnMozoDia.Location = new System.Drawing.Point(14, 224);
+            this.btnMozoDia.Name = "btnMozoDia";
+            this.btnMozoDia.Size = new System.Drawing.Size(106, 41);
+            this.btnMozoDia.TabIndex = 3;
+            this.btnMozoDia.Text = "Mozo del Día";
+            this.btnMozoDia.UseVisualStyleBackColor = true;
+            this.btnMozoDia.Click += new System.EventHandler(this.btnMozoDia_Click);
+            // 
+            // txtMozoDia
+            // 
+            this.txtMozoDia.Location = new System.Drawing.Point(126, 235);
+            this.txtMozoDia.Name = "txtMozoDia";
+            this.txtMozoDia.Size = new System.Drawing.Size(124, 20);
+            this.txtMozoDia.TabIndex = 4;
+            // 
+            // lstTotales
+            // 
+            this.lstTotales.FormattingEnabled = true;
+            this.lstTotales.Location = new System.Drawing.Point(267, 271);
+            this.lstTotales.Name = "lstTotales";
+            this.lstTotales.Size = new System.Drawing.Size(246, 147);
+            this.lstTotales.TabIndex = 5;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(440, 427);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 462);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lstTotales);
+            this.Controls.Add(this.txtMozoDia);
+            this.Controls.Add(this.btnMozoDia);
+            this.Controls.Add(this.btnTotales);
+            this.Controls.Add(this.btnValidarDatos);
+            this.Controls.Add(this.dgvMozos);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Ventas Diarias - La Milanga";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMozos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMozos;
+        private System.Windows.Forms.Button btnValidarDatos;
+        private System.Windows.Forms.Button btnTotales;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mozo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Comidas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Bebidas_sin_alcohol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Bebidas_con_alcohol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Postres;
+        private System.Windows.Forms.Button btnMozoDia;
+        private System.Windows.Forms.TextBox txtMozoDia;
+        private System.Windows.Forms.ListBox lstTotales;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ pryComettoSP4/frmMain.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryComettoSP4
+{
+    public partial class frmMain : Form
+    {
+        public float[,] matImportes = new float[6,5];
+        public void llenarMatriz()
+        {
+            for (int f = 0; f < matImportes.GetLength(0) - 1; f++)
+            {
+                for (int c = 0; c < matImportes.GetLength(1) - 1; c++)
+                {
+                    matImportes[f, c] = Convert.ToSingle(dgvMozos.Rows[f].Cells[c + 1].Value);
+                }
+            }
+
+        }
+        public void dgvMozosLoad()
+        {
+            dgvMozos.Rows.Add();
+            dgvMozos[0, 0].Value = "Julio";
+            dgvMozos.Rows.Add();
+            dgvMozos[0, 1].Value = "Esteban";
+            dgvMozos.Rows.Add();
+            dgvMozos[0, 2].Value = "Javier";
+            dgvMozos.Rows.Add();
+            dgvMozos[0, 3].Value = "Gonzalo";
+            dgvMozos.Rows.Add();
+            dgvMozos[0, 4].Value = "Alberto";
+            for (int c = 1; c < dgvMozos.ColumnCount; c++)
+            {
+                for (int f = 0; f < dgvMozos.Rows.Count; f++)
+                {
+                    if (dgvMozos[c, f].Value == null)
+                    {
+                        dgvMozos[c, f].Value = 0;
+                    }
+                }
+            }
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            dgvMozosLoad();
+        }
+
+        private void btnValidarDatos_Click(object sender, EventArgs e)
+        {
+            lstTotales.Items.Clear();
+            txtMozoDia.Text = "";
+            Array.Clear(matImportes,0,matImportes.Length);
+            bool datosValidados = true;
+            for (int c = 1; c < dgvMozos.ColumnCount; c++)
+            {
+                for (int r = 0; r < dgvMozos.Rows.Count; r++)
+                {
+                    if (dgvMozos[c, r].Value == null)
+                    {
+                        datosValidados = false;
+                        MessageBox.Show("Faltan datos en:\nMozo: " + dgvMozos[0, r].Value + ", Categoría: " + dgvMozos.Columns[c].Name, "Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    }
+                }
+            }
+            if (datosValidados == true)
+            {
+                MessageBox.Show("Datos Validados Correctamente!","Éxito",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                btnTotales.Enabled = true;
+            }
+            llenarMatriz();
+            for (int f = 0; f < matImportes.GetLength(0) - 1; f++)
+            {
+                for (int c = 0; c < matImportes.GetLength(1) - 1; c++)
+                {
+                    matImportes[f, 4] = matImportes[f, 4] + matImportes[f, c];
+                }
+            }
+            for (int c = 0; c < matImportes.GetLength(1) - 1; c++)
+            {
+                for (int f = 0; f < matImportes.GetLength(0) - 1; f++)
+                {
+                    matImportes[5, c] = matImportes[5, c] + matImportes[f, c];
+                }
+            }
+
+            btnMozoDia.Enabled= true;
+            btnTotales.Enabled= true;
+        }
+
+        private void btnTotales_Click(object sender, EventArgs e)
+        {
+            lstTotales.Items.Clear();
+            lstTotales.Items.Add("Totales de Mozos");
+            lstTotales.Items.Add("Total Julio: $" + matImportes[0, 4]);
+            lstTotales.Items.Add("Total Esteban: $" + matImportes[1, 4]);
+            lstTotales.Items.Add("Total Javier: $" + matImportes[2, 4]);
+            lstTotales.Items.Add("Total Gonzalo: $" + matImportes[3, 4]);
+            lstTotales.Items.Add("Total Alberto: $" + matImportes[4, 4]);
+            lstTotales.Items.Add("─────────────────────────");
+            lstTotales.Items.Add("Totales de Categorías");
+            lstTotales.Items.Add("Total Comidas: $" + matImportes[5, 0]);
+            lstTotales.Items.Add("Total Bebidas sin alcohol: $" + matImportes[5, 1]);
+            lstTotales.Items.Add("Total Bebidas con alcohol: $" + matImportes[5, 2]);
+            lstTotales.Items.Add("Total Postres: $" + matImportes[5, 3]);
+            btnTotales.Enabled= false;
+        }
+
+        private void btnMozoDia_Click(object sender, EventArgs e)
+        {
+            float totalMayor = matImportes[0,0];
+            int indice = 0;
+            for (int f = 0; f < matImportes.GetLength(0) - 1; f++)
+            {
+                if (totalMayor < matImportes[f,4])
+                {
+                    totalMayor = matImportes[f, 4];
+                    indice = f;
+                }
+            }
+            if (indice == 0)
+            {
+                txtMozoDia.Text = "Julio - $" + matImportes[0, 4];
+            }
+            else
+            {
+                if (indice == 1)
+                {
+                    txtMozoDia.Text = "Esteban - $" + matImportes[1, 4];
+                }
+                else
+                {
+                    if (indice == 2)
+                    {
+                        txtMozoDia.Text = "Javier - $" + matImportes[2, 4];
+                    }
+                    else
+                    {
+                        if (indice == 3)
+                        {
+                            txtMozoDia.Text = "Gonzalo - $" + matImportes[3, 4];
+                        }
+                        else
+                        {
+                            if (indice == 4)
+                            {
+                                txtMozoDia.Text = "Alberto - $" + matImportes[4, 4];
+                            }
+                        }
+                    }
+                }
+            }
+            btnMozoDia.Enabled = false;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
